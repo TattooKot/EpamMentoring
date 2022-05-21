7e5029d56fd9d17c2ed2decc8746fc347517e369
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-05-21 08:29:59 +0300

    Testing variables: fail tests added

--- tests/src/test/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ tests/src/test/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,19 @@
 package com.epam.ld.module2.testing.template;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TemplateEngineTest {
+
+    @Test
+    public void setTemplateVariables(){
+        fail();
+    }
+
+    @Test(expected = ClassFormatError.class)
+    public void testTemplateNullValue(){
+        fail();
+    }
+
 }
