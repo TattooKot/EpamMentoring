f69d613021d676cbf3751362c9592683d02646b3
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-05-21 09:48:55 +0300

    Separated TemplateEngine and Template tests

--- tests/src/test/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ tests/src/test/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,54 +1,5 @@
 package com.epam.ld.module2.testing.template;
 
-import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.InvalidPropertiesFormatException;
-import java.util.List;
-
-import static org.junit.Assert.*;
-
 public class TemplateEngineTest {
-
-    @Test
-    public void setTemplateVariables() throws InvalidPropertiesFormatException {
-        Template template = new Template("#{subject}");
-        List<String> tags = new ArrayList<>();
-        tags.add("tag");
-        String result = template.getResultString(tags);
-        assertNotNull(result);
-    }
-
-    @Test(expected = InvalidPropertiesFormatException.class)
-    public void testTemplateNullValue() throws InvalidPropertiesFormatException {
-        Template template = new Template("");
-        template.getResultString(null);
-    }
-
-
-
-    @Test(expected = InvalidPropertiesFormatException.class)
-    public void testTemplateNotEnoughTags() throws InvalidPropertiesFormatException {
-        Template template = new Template(" #{subject}  #{subject}  #{subject}  #{subject}");
-        List<String> tags = new ArrayList<>();
-        tags.add("tag");
-        tags.add("tag");
-        tags.add("tag");
-        template.getResultString(tags);
-    }
-
-    @Test
-    public void moreThatExpectedTags() throws InvalidPropertiesFormatException {
-        Template template = new Template("#{subject} #{subject} #{subject}");
-        List<String> tags = new ArrayList<>();
-        tags.add("tag");
-        tags.add("tag");
-        tags.add("tag");
-        tags.add("tag");
-        tags.add("tag");
-        tags.add("tag");
-        String resultString = template.getResultString(tags);
-        assertNotNull(resultString);
-    }
 
 }
