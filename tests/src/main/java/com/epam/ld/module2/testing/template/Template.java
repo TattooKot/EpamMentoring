3ac04e6da603638596e9660337e4f97dbc139037
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-05-21 08:42:24 +0300

    Testing variables: minimum code added to pass first test

--- tests/src/main/java/com/epam/ld/module2/testing/template/Template.java
+++ tests/src/main/java/com/epam/ld/module2/testing/template/Template.java
@@ -1,8 +1,13 @@
 package com.epam.ld.module2.testing.template;
 
+import java.util.List;
+
 /**
  * The type Template.
  */
 public class Template {
 
+    public String getResultString(List<String> tags) {
+        return "";
+    }
 }
--- tests/src/test/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ tests/src/test/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -2,13 +2,19 @@ package com.epam.ld.module2.testing.template;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class TemplateEngineTest {
 
     @Test
     public void setTemplateVariables(){
-        fail();
+        Template template = new Template();
+        List<String> tags = new ArrayList<>();
+        String result = template.getResultString(tags);
+        assertNotNull(result);
     }
 
     @Test(expected = ClassFormatError.class)
