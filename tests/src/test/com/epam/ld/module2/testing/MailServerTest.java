93e09e0d1d79465dbe0e3887c8008a3bd0250f5f
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-05-23 13:55:50 +0300

    First failed test from MailServerTest

--- tests/src/test/com/epam/ld/module2/testing/MailServerTest.java
+++ tests/src/test/com/epam/ld/module2/testing/MailServerTest.java
@@ -1,4 +1,13 @@
 package com.epam.ld.module2.testing;
 
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
 public class MailServerTest {
+
+    @Test
+    public void test(){
+        fail();
+    }
 }
