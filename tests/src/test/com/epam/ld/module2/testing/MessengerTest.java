3dc8c20f13723eba6aadcf2ddf0e210362eaa65d
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-05-23 10:50:14 +0300

    First failed test from MessengerTest

---
+++ tests/src/test/com/epam/ld/module2/testing/MessengerTest.java
@@ -0,0 +1,13 @@
+package com.epam.ld.module2.testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class MessengerTest {
+
+    @Test
+    public void test() {
+        fail();
+    }
+}
