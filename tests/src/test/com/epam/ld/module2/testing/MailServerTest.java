03771b4f4034b8f87daa1936960e7b19b18d9cd7
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-05-23 14:03:33 +0300

    MailServerTest validInfoTest implemented

--- tests/src/test/com/epam/ld/module2/testing/MailServerTest.java
+++ tests/src/test/com/epam/ld/module2/testing/MailServerTest.java
@@ -1,13 +1,31 @@
 package com.epam.ld.module2.testing;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.InvalidPropertiesFormatException;
 
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class MailServerTest {
 
+    private final String address = "addresses";
+    private final String message = "message";
+
+    @Mock
+    MailServer server;
+
+    @Before
+    public void setUp(){
+        server = mock(MailServer.class);
+    }
+
     @Test
-    public void test(){
-        fail();
+    public void validInfoTest() throws InvalidPropertiesFormatException {
+        server.send(address, message);
+        verify(server).send(address, message);
     }
 }
