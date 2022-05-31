a37fbbc5e205a25b419a268a38cb1eecd959e972
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-05-31 14:37:20 +0300

    Console gui added

--- files/src/main/java/Task2.java
+++ files/src/main/java/Task2.java
@@ -1,20 +1,55 @@
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
+//C:\Users\Stanislav_Kotsaha\Desktop\New folder\EpamMentoring
+
 public class Task2 {
+
+    public static final String RESULT_FILE = "files\\result.txt";
+
     public static void main(String[] args) {
-//        System.out.println(findFileWithMaxSLettersInName("C:\\Users\\Stanislav_Kotsaha\\Desktop\\New folder\\EpamMentoring"));
-//        System.out.println(getAverageSizeOfFiles("C:\\Users\\Stanislav_Kotsaha\\Desktop\\New folder\\EpamMentoring"));
-//        System.out.println(getFiveLargestFiles("C:\\Users\\Stanislav_Kotsaha\\Desktop\\New folder\\EpamMentoring"));
-        System.out.println(getNumberOfFilesAndFoldersByLetter("C:\\Users\\Stanislav_Kotsaha\\Desktop\\New folder\\EpamMentoring", "s"));
+
+        System.out.println("Select function number:");
+        System.out.println("1. Find file with max 's' letter in name");
+        System.out.println("2. Get average size of file");
+        System.out.println("3. Get five largest files");
+        System.out.println("4. Get number of files and folders by letter");
+
+        Scanner scanner = new Scanner(System.in);
+        int numOfFunction = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter path:");
+        String path = scanner.nextLine();
+
+        try {
+            Paths.get(path);
+        } catch (InvalidPathException | NullPointerException ex) {
+            System.out.println("Invalid path");
+            return;
+        }
+
+        switch (numOfFunction){
+            case 1 -> findFileWithMaxSLettersInName(path);
+            case 2 -> getAverageSizeOfFiles(path);
+            case 3 -> getFiveLargestFiles(path);
+            case 4 -> {
+                System.out.println("Write letter for filter:");
+                String letter = scanner.nextLine();
+                getNumberOfFilesAndFoldersByLetter(path, letter);
+            }
+        }
+
+        System.out.println("Result here: " + RESULT_FILE);
     }
 
-    public static Map<String, Long> getNumberOfFilesAndFoldersByLetter(String path, String letter){
+    public static void getNumberOfFilesAndFoldersByLetter(String path, String letter){
         List<Path> allPaths = getAllPaths(path);
 
         long numberOfFolders = allPaths.stream()
@@ -31,13 +66,14 @@ public class Task2 {
         result.put("Folders", numberOfFolders);
         result.put("Files", numberOfFiles);
 
-        return result;
+        writeResultToFile("Number of files and folders: " + result);
     }
 
-    public static List<String> getFiveLargestFiles(String path){
+    public static void getFiveLargestFiles(String path){
         List<Path> pathList = getAllPaths(path);
 
         List<String> list = pathList.stream()
+                .filter(Files::isRegularFile)
                 .sorted(Comparator.comparing((file) -> {
                     try {
                         return Files.size(file);
@@ -51,16 +87,16 @@ public class Task2 {
         Collections.reverse(list);
 
         if(list.size() > 5){
-            return list.subList(0, 5);
+            list = list.subList(0, 5);
         }
 
-        return list;
+        writeResultToFile("File largest files: " + list);
     }
 
-    public static Double getAverageSizeOfFiles(String path) {
+    public static void getAverageSizeOfFiles(String path) {
         List<Path> pathList = getAllPaths(path);
 
-        return pathList.stream()
+        double result = pathList.stream()
                 .mapToDouble(filePath -> {
                     try {
                         return Files.size(filePath);
@@ -70,15 +106,19 @@ public class Task2 {
                 })
                 .average()
                 .orElse(Double.NaN);
+
+        writeResultToFile("Average file size: " + result);
     }
 
-    public static String findFileWithMaxSLettersInName(String path) {
+    public static void findFileWithMaxSLettersInName(String path) {
         List<List<Character>> filesNames = getListWithFileNames(path);
         List<Character> fileWithMaxSLetters = getFileNameWithMaxSLetters(filesNames);
 
-        return fileWithMaxSLetters.stream()
+        String result = fileWithMaxSLetters.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining());
+
+        writeResultToFile("File with max 's' letters: " + result);
     }
 
     private static List<List<Character>> getListWithFileNames(String path) {
@@ -105,5 +145,13 @@ public class Task2 {
         return filesList.stream()
                 .max(Comparator.comparing(word -> Collections.frequency(word, 's')))
                 .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private static void writeResultToFile(String result){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(RESULT_FILE))){
+            writer.write(result);
+        } catch (IOException e){
+            System.out.println("Error write writing result to file");
+        }
     }
 }
