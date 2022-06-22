854a8a8acec8c83c6e1cbaa36908bbe695fab123
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-06-22 09:24:47 +0300

    Task7 DBUtils added

---
+++ files-Task7/src/main/java/Utils/DBUtils.java
@@ -0,0 +1,28 @@
+package Utils;
+
+import java.sql.*;
+
+public class DBUtils {
+    private static final String URL = "jdbc:postgresql://localhost/task7";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "password";
+
+
+    public PreparedStatement getStatement(String sql){
+        try {
+            return getConnection().prepareCall(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        throw  new RuntimeException();
+    }
+
+    private Connection getConnection() {
+        try {
+            return  DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException();
+    }
+}
