7fcccc10ff129bdd60f3768adfd02a64cebf9a82
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-06-22 09:13:57 +0300

    Task7 init commit

--- files-Task7/src/main/java/Task7.java
+++ files-Task7/src/main/java/Task7.java
@@ -2,8 +2,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "files-Task7")
 public class Task7 {
 
 	public static void main(String[] args) {
--- files-Task7/src/main/java/repo/FileFromDBRepository.java
+++
@@ -1,9 +0,0 @@
-package repo;
-
-import model.FileFromDB;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface FileFromDBRepository extends JpaRepository<FileFromDB, Integer> {
-}
