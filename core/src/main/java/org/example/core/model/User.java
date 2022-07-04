0d3bbc07e6ec04a011dc279a9e784dc00d2c5b50
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-07-04 09:19:59 +0300

    User model added

---
+++ core/src/main/java/org/example/core/model/User.java
@@ -0,0 +1,11 @@
+package org.example.core.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer phone;
+}
