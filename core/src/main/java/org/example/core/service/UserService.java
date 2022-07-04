7fbe570ab757a169288842f11673b2ce69144b8a
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-07-04 17:21:32 +0300

    UserService added

---
+++ core/src/main/java/org/example/core/service/UserService.java
@@ -0,0 +1,30 @@
+package org.example.core.service;
+
+import org.example.core.model.User;
+import org.example.core.repository.UserRepo;
+
+import java.util.Map;
+
+public class UserService {
+    private final UserRepo userRepo = new UserRepo();
+
+    public Map.Entry<Integer, User> save(User user) {
+        return userRepo.save(user).orElseGet(() -> Map.entry(0, null));
+    }
+
+    public Map.Entry<Integer, User> getById(Integer id) {
+        return userRepo.getById(id).orElseGet(() -> Map.entry(0, null));
+    }
+
+    public Map<Integer, User> getAll() {
+        return userRepo.getAll();
+    }
+
+    public Map.Entry<Integer, User> update(User user) {
+        return userRepo.update(user).orElseGet(() -> Map.entry(0, null));
+    }
+
+    public void deleteById(Integer id) {
+        userRepo.deleteById(id);
+    }
+}
