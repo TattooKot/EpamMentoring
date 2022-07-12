c5e251c333942fd73256042f4c885d7906809063
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-07-12 12:54:06 +0300

    Ticket entity updated

--- core/src/main/java/org/example/core/model/Ticket.java
+++ core/src/main/java/org/example/core/model/Ticket.java
@@ -6,56 +6,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Ticket implements TicketInterface {
-    private Long id;
-    private Long eventId;
-    private Long userId;
+    private long id;
+    private long eventId;
+    private long userId;
     private Category category;
-    private Integer place;
+    private int place;
 
     public Ticket(Long eventId, Long userId, Category category, Integer place) {
         this.eventId = eventId;
         this.userId = userId;
         this.category = category;
-        this.place = place;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(long id) {
-        eventId = id;
-    }
-
-    public long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(long id) {
-        userId = id;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public int getPlace() {
-        return place;
-    }
-
-    public void setPlace(int place) {
         this.place = place;
     }
 }
