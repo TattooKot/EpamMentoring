35079ca11cd8fac45a639441bc6754c011f73cf5
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-07-28 15:19:45 +0300

    Welcome page added

--- springwebmvc/src/main/java/controller/RestCont.java
+++ springwebmvc/src/main/java/controller/RestCont.java
@@ -1,33 +1,43 @@
 package controller;
 
 import model.User;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @Controller
 public class RestCont {
 
-    @GetMapping("/test")
-    public void test(HttpServletResponse response) throws IOException {
-        response.setContentType("text");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter writer = response.getWriter();
-        writer.println("{\"Hello world from events!\"}");
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+
+
+    @GetMapping("/addUser")
+    public String getAccountForm() {
+        return "userForm";
     }
 
     @GetMapping("/user")
     public String user(Model model) throws IOException {
         User user = new User();
-        user.setEmail("email@epam.com");
-        user.setName("First user");
-        user.setPhone(123456789);
-        System.out.println(user);
+//        user.setEmail("email@epam.com");
+//        user.setName("First user");
+//        user.setPhone(123456789);
+//        System.out.println(user);
 
+        model.addAttribute("user", user);
+        return "user";
+    }
+
+    @PostMapping("/user")
+    public String createUser(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) User user, Model model) {
+        System.out.println("User from form: " + user);
         model.addAttribute("user", user);
         return "user";
     }
