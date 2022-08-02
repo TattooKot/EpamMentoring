3be94e6c2d8c5224179aa20ca9de8950634479de
Stanislav Kotsaha <stanislav_kotsaha@epam.com>
2022-08-02 14:46:44 +0300

    Get pdf with list of events by date added

--- springwebmvc/src/main/java/controller/FileRestControllerV1.java
+++ springwebmvc/src/main/java/controller/FileRestControllerV1.java
@@ -7,6 +7,7 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -35,6 +37,18 @@ public class FileRestControllerV1 {
         List<Event> eventsByTitle = bookingFacade.getEventsByTitle(title, size, num);
         System.out.println(eventsByTitle);
         return createPdf(eventsByTitle.toString());
+    }
+
+    @GetMapping("/event")
+    @ResponseBody
+    public ResponseEntity<byte[]> eventByDate(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date date,
+            @RequestParam Integer size,
+            @RequestParam Integer num)
+            throws IOException {
+        List<Event> eventsForDay = bookingFacade.getEventsForDay(date, size, num);
+        System.out.println(eventsForDay);
+        return createPdf(eventsForDay.toString());
     }
 
     @GetMapping("/file")
